fbaf5c3f53b18d8f1c87b9029a3c786882f068ea
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2023-02-10 23:42:39 +0100

    Initial commit + first ex

---
+++ blockchain-cryptography/bruteforce-hashing/index.js
@@ -0,0 +1,16 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+// the possible colors that the hash could represent
+const COLORS = ["red", "green", "blue", "yellow", "pink", "orange"];
+
+// given a hash, return the color that created the hash
+function findColor(hash) {
+  for (const color of COLORS) {
+    if (toHex(sha256(utf8ToBytes(color))) === toHex(hash)) {
+      return color;
+    }
+  }
+}
+
+module.exports = findColor;
---
+++ blockchain-cryptography/bruteforce-hashing/test/mytest.js
@@ -0,0 +1,14 @@
+const { assert } = require("chai");
+const findColor = require("../index");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+const { sha256 } = require("ethereum-cryptography/sha256");
+
+const COLORS = ["red", "green", "blue", "yellow", "pink", "orange"];
+
+describe("findColor", () => {
+  COLORS.forEach((color) => {
+    it(`should work for ${color}`, () => {
+      assert.equal(findColor(sha256(utf8ToBytes(color))), color);
+    });
+  });
+});
